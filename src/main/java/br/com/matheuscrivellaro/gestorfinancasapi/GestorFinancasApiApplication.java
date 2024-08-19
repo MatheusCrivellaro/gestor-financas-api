4d3774bd5f68c1cb87c088be4034526a2ef519ac
MatheusCrivellaro <matheuscriv@gmail.com>
2024-08-18 23:14:44 -0300

    Initial commit

---
+++ src/main/java/br/com/matheuscrivellaro/gestorfinancasapi/GestorFinancasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheuscrivellaro.gestorfinancasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorFinancasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorFinancasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheuscrivellaro/gestorfinancasapi/GestorFinancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheuscrivellaro.gestorfinancasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorFinancasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
